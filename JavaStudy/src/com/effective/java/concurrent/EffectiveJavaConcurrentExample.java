83b665c6f0452221371de81448d62e80a0952c47
unseok_kim <unseok_kim@tmax.co.kr>
2017-05-31 16:08:52 +0900

    [Concurrent] volatile에 대한 오해 generateSerialNumber

--- JavaStudy/src/com/effective/java/concurrent/EffectiveJavaConcurrentExample.java
+++ JavaStudy/src/com/effective/java/concurrent/EffectiveJavaConcurrentExample.java
@@ -11,13 +11,78 @@
 
 package com.effective.java.concurrent;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
 /**
  * @author unseok.kim
- * @since  2017. 5. 31.
+ * @since 2017. 5. 31.
  */
 public class EffectiveJavaConcurrentExample {
 
-    
-    
-}
+    private static volatile int nextSerialNumber = 0;
 
+    public static int generateSerialNumber() {
+        return nextSerialNumber++;
+    }
+
+    @Test
+    public void 단일스레드테스트() {
+        for (int forIdx = 0; forIdx < 10000; forIdx++) {
+            int idx = generateSerialNumber();
+            System.out.println("idx : " + idx);
+        }
+        
+        int idx = generateSerialNumber();
+        System.out.println("Lastidx : " + idx);
+
+    }
+
+    @Test
+    public void 멀티스레드테스트() throws InterruptedException {
+        ExecutorService exService = Executors.newFixedThreadPool(1000);
+        for (int forIdx = 0; forIdx < 10000; forIdx++) {
+            
+            exService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    int idx = generateSerialNumber();
+                    System.out.println("idx : " + idx);
+                }
+            });
+            /*
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    
+                    int idx = generateSerialNumber();
+                    System.out.println("idx : " + idx);
+                }
+            }).start();
+            */
+        }
+        exService.shutdown();
+        exService.awaitTermination(10, TimeUnit.SECONDS);
+//        exService.shutdown();
+        int idx = generateSerialNumber();
+        Thread.sleep(500);
+        System.out.println("Lastidx : " + idx);
+
+    }
+}
